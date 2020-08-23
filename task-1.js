0808377500280d4a28533d067cec6023c99e7097
Ruslan Agaverdiev <rusl21ak@gmail.com>
2020-08-23 19:42:57 +0300

    HW2

---
+++ task-1.js
@@ -0,0 +1,13 @@
+const getItemsString = function(array) {
+    'use strict';
+
+    let result= ""
+    
+    for(let i = 0; i < array.length; i += 1) {
+    
+      result += `${i+1} - ${array[i]}\n` ;
+    }
+  return result
+  };
+  
+  console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
---
+++ task-5.js
@@ -0,0 +1,16 @@
+function checkForSpam (str) { 
+    'use strict';
+    // Write code under this line
+    if (
+   str.toLowerCase().includes("sale") || str.toLowerCase().includes("spam")) {
+      return true;
+    } else return false;
+  }
+  
+  console.log(checkForSpam('Latest technology news')); // false
+  
+  console.log(checkForSpam('JavaScript weekly newsletter')); // false
+  
+  console.log(checkForSpam('Get best sale offers now!')); // true
+  
+  console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
